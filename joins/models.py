0c8ea18ad33fd992a56493ea84e0ef8e14cf7055
da-ta-vinci21 <akashtndn.acm@gmail.com>
2014-11-11 19:52:05 +0530

    Created unique reference id; added custom middleware to capture it from the incoming request

--- joins/models.py
+++ joins/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 class join(models.Model):
-	email = models.EmailField(unique = True)
+	email = models.EmailField()
+	ref_id = models.CharField(max_length = 120, default = 'PQR')
 	ip_address = models.CharField(max_length =  120, default = 'ABC')
 	timestamp = models.DateTimeField(auto_now = True, auto_now_add = False)
 	update = models.DateTimeField(auto_now = False, auto_now_add = True)
 
 	def __unicode__(self):
 		return self.email
+
+	class Meta():
+		unique_together = ("email","ref_id",)
--- joins/views.py
+++ joins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 from .forms import EmailForm, joinForm
 from .models import join
@@ -15,7 +15,31 @@ from .admin import get_ip
 # 		ip = ""
 # 	return ip
 
+import uuid
+
+def get_ref_id():
+	ref_id = str(uuid.uuid4())[:11].replace('-', '').lower()
+	try:
+		id_exists = join.objects.get(ref_id=ref_id)
+		get_ref_id()	
+	except:
+		return ref_id
+
+def share(request, ref_id):
+	#print ref_id
+	context = {"ref_id": ref_id}
+	template = "share.html"
+	return render(request, template, context)
+	
+
 def home(request):
+	try:
+		join_id = request.session['join_id_ref']
+		obj = join.objects.get(id = join_id)
+	except:
+		join_id = None
+
+	print join_id
 	# form = EmailForm(request.POST or None)
 	# if form.is_valid():
 	# 	email = form.cleaned_data['email']
@@ -27,8 +51,10 @@ def home(request):
 		email = form.cleaned_data['email']
 		new_join_old, created = join.objects.get_or_create(email = email)
 		if created:
+			new_join_old.ref_id = get_ref_id()
 			new_join_old.ip = get_ip(request)
 			new_join_old.save()
+		return HttpResponseRedirect("/%s" %(new_join_old.ref_id))
 		# new_join.ip = get_ip(request)
 		# new_join.save()
 		# print new_join_old.timestamp
